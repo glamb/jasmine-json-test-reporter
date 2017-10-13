ba8a0665b706c03474b9e512142a977341791598
Gregg Lamb <greggmlamb@gmail.com>
2017-10-12 21:37:58 -0700

    stats file to display totals

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var fs = require('fs')
 
+
+
 var defaultOpts = {
 	file: 'jasmine-test-results.json',
+	stats: 'stats.json',
 	beautify: true,
 	indentationLevel: 4 // used if beautify === true
 };
@@ -10,15 +13,25 @@ function reporter(opts) {
 	var options = shallowMerge(defaultOpts, typeof opts === 'object' ? opts : {});
 	var specResults = [];
 	var masterResults = Object.create(null);
+	var failed = 0;
+	var passed = 0;
+	var skipped = 0;
+	var specs = 0
+	var suites = 0
 
 	this.suiteDone = function(suite) {
 		suite.specs = specResults;
 		masterResults[suite.id] = suite;
 		specResults = [];
+		suites++;
 	};
 
 	this.specDone = function(spec) {
 		specResults.push(spec);
+		specs++
+		if (spec.status === 'passed') passed++
+		if (spec.status === 'failed') failed++
+		if (spec.status === 'skipped') skipped++
 	};
 
 	this.jasmineDone = function() {
@@ -26,6 +39,19 @@ function reporter(opts) {
 			JSON.stringify(masterResults, null, options.indentationLevel) :
 			JSON.stringify(masterResults);
 
+		var stats = {
+			passed: passed,
+			failed: failed,
+			skipped: skipped,
+			specs: specs,
+			suites: suites
+		}
+
+		var statsOutput = options.beautify ?
+			JSON.stringify(stats, null, options.indentationLevel) :
+			JSON.stringify(stats);
+
+		fs.writeFileSync(options.stats, statsOutput);
 		fs.writeFileSync(options.file, resultsOutput);
 	};
 };
